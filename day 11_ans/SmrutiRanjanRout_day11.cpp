f6a159a5e39eda39dbf958f817b02767222fce26
Smruti <smrutiranjanrout2019@gmail.com>
2021-10-01 13:46:04 +0530

    adding day 11 ans

---
+++ day 11_ans/SmrutiRanjanRout_day11.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+//-------------------------------------------------------------------------------------------------
+#define ll long long
+#define nl cout << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define vl vector<ll>
+#define pll pair<ll, ll>
+#define mll map<ll, ll>
+#define msl map<string, ll>
+#define mls map<ll, string>
+#define umll unordered_map<ll, ll>
+#define umsl unordered_map<string, ll>
+#define umls unordered_map<ll, string>
+#define sl set<ll>
+#define ss set<string>
+#define usl unordered_set<ll>
+#define uss unordered_set<string>
+#define in(n) cin >> n
+#define in2(n, m) cin >> n >> m
+#define sizear(arr) sizeof(arr) / sizeof(arr[0]);
+#define fo(i, a, n) for (ll i = a; i < n; i++)
+//-------------------------------------------------------------------------------------------------
+int count_f(int n, int count)
+{
+    if (n == 1)
+    {
+        return count;
+    }
+    count++;
+    return count_f(n / 2, count);
+}
+void solution()
+{
+    ll n;
+    cin >> n;
+    ll count = 0;
+    cout << count_f(n, count) << endl;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("SmrutiRanjanRout_day11_output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solution();
+    }
+
+    return 0;
+}
